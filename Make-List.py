a125df07cb3eabb85ff8820ac8d074f8a58a3475
taiz733 <76573036+taiz733@users.noreply.github.com>
2020-12-25 21:02:55 +0300

    Add files via upload

---
+++ Make-List.py
@@ -0,0 +1,13 @@
+#coded by:@VIGITAMHS
+print ("Coded by:\n=>VIGITA_MHS")
+def ViGiTa_MhS():
+    password = open("vigitamhs.txt","w")
+    print ("[+] - please wite")
+    i =100000000
+    while i<=999999999:
+        i+=1
+        password.write (str(i))
+        password.write ("\n")   
+    password.close()
+    print ("My channel in telegram:\nhttps://ERROR404VIGITAMHS")
+ViGiTa_MhS()
